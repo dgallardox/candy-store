f8408b72f7ad9e3395130afcf5a7d629d770f316
Diego J.  Gallardo <101141475+dgallardox@users.noreply.github.com>
2024-07-31 00:50:48 +0000

    added pattern that matches index.php?graphql

--- scripts.sh
+++ scripts.sh
@@ -1,12 +1,11 @@
 this=$(basename "$(pwd)")
 
-function httpmeth(){
+httpmeth(){
   echo "Getting the HTTP methods used to make a request to $2"
   if [[ "$2" == "graphql" ]]; then
-    pattern="/graphql|/index.php?graphql"
+    pattern="(/index\.php\?graphql|/graphql)"
   else
     pattern="$2"
   fi
-  cat $nginx_log_path/$1.access.log | grep -E "$pattern" | cut -d'|' -f10 | awk '{print $1}' | sort | uniq -c
+  cat "$nginx_log_path/$1.access.log.1" | grep -E "$pattern" | cut -d'|' -f10 | awk '{print $1}' | sort | uniq -c
 }
-
